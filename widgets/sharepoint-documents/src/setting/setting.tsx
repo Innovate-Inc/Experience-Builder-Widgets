43ebe8aa3e6b8976a8244883491eaedc9df235b1
Travis Bock <tbock@innovateteam.com>
2022-06-16 09:14:28 -0700

    allow map service type in select data

--- widgets/sharepoint-documents/src/setting/setting.tsx
+++ widgets/sharepoint-documents/src/setting/setting.tsx
@@ -29,7 +29,7 @@ interface State {
 }
 
 export default class Setting extends React.PureComponent<AllWidgetSettingProps<Config>, State> {
-  supportedTypes = Immutable([DataSourceTypes.FeatureLayer]);
+  supportedTypes = Immutable([DataSourceTypes.FeatureLayer, DataSourceTypes.MapService]);
 
   state = {currentDS: null, expressionOpen: false};
   currentExpression = null
